9cbf6cade77dc1cd9d355e56d5ca3276741c724e
rajithaeyee <rajithaeye@gmail.com>
2018-09-16 21:13:25 +0530

    Added beer model

---
+++ src/models/beer.ts
@@ -0,0 +1,19 @@
+import {Volume} from './volume';
+
+export interface Beer{
+    id:number;
+    name:string;
+    first_brewed:string;
+    description:string;
+    image_url:string;
+    abv:number;
+    ibu:number;
+    target_fg:number;
+    target_og:number;
+    ebc:number;
+    srm:number;
+    ph:number;
+    attenuation_level:number;
+    volume:Volume;
+    boil_volume:Volume;
+}
